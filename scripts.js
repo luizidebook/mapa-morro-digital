29ea6a8773ef47535859a1f0e60a89da7370cad6
Luiz Idebook <luizidebook@gmail.com>
2025-03-30 12:11:52 -0300

    Descrição das alterações

--- scripts.js
+++ scripts.js
@@ -1,7 +1,8 @@
 // Importações do módulo core/config.js
-import { initializeMap, showWelcomeMessage } from './js/map/map-core.js';
-import { autoAdjustTheme } from './js/ui/theme.js';
+import { initializeMap } from './js/map/map-core.js';
 import { setupEventListeners } from './js/core/event-listeners.js';
+import { autoAdjustTheme } from './js/ui/theme.js';
+import { showWelcomeMessage } from './js/core/config.js';
 
 // 1. onDOMContentLoaded       - Executado quando o DOM carrega (busca parâmetros iniciais)*/
 document.addEventListener('DOMContentLoaded', () => {
